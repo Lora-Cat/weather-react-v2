1a56aaff60d21692830a10b988015e012468e92b
Valentyna Nykonchuk <69204446+Lora-Cat@users.noreply.github.com>
2022-11-20 21:01:05 +0200

    Added github link

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,12 @@
+/** @format */
+
 import "./Footer.css";
 
 export default function Footer() {
   return (
     <footer>
       <a
-        href="https://github.com/Lora-Cat/weather"
+        href="https://github.com/Lora-Cat/weather-react-v2"
         target="_blank"
         rel="nofollow noreferrer"
         className="text-decoration-none"
